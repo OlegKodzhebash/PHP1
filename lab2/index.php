d028dbb3981448f40b1cdcfa4ea74cb4a1551e13
Kodzhebash Oleg <oleg.cela@gmail.com>
2026-02-19 10:22:40 +0200

    added lab1 and lab2

---
+++ lab2/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Привет, мир!"; 
+
+echo "Hello, World with echo!";
+print "Hello, World with print! <br />" ;
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo "Прошло " . $days . " дней. " . $message . "<br />";
+
+echo "Прошло $days дней. $message<br />";
+
+echo $days . " — количество дней. " . $message . "<br />";
+?>
